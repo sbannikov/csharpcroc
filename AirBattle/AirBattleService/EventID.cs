58c3d89234653b93712f4a4d32a4ae26e4213b3f
Sergey Bannikov <6384@croc.ru>
2019-04-26 20:38:57 +0300

    Сервис операционной системы

---
+++ AirBattle/AirBattleService/EventID.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirBattle
+{
+    /// <summary>
+    /// Идентификаторы события в журнале Windows
+    /// </summary>
+    public enum  EventID
+    {
+        None,
+        Start,
+        Stop,
+        Pause,
+        Continue,
+        Exception,
+        Timer,
+        Watch
+    }
+}
---
+++ AirBattle/AirBattleService/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Configuration.Install;
+using System.Reflection;
+
+namespace AirBattle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                GameService svc;
+
+                // Первый параметр командной строки
+                string arg1 = (args.Count() > 0) ? args[0] : null;
+                // Имя испольняемого файла сервиса
+                string name = Assembly.GetExecutingAssembly().Location;
+
+                switch (arg1)
+                {
+                    case null:
+                        svc = new GameService();
+                        ServiceBase.Run(svc);
+                        break;
+
+                    case "install":
+                        ManagedInstallerClass.InstallHelper(new string[] { name });
+                        break;
+
+                    case "uninstall":
+                        ManagedInstallerClass.InstallHelper(new string[] { "/u", name });
+                        break;
+
+                    case "console":
+                        svc = new GameService();
+                        svc.Start();
+                        Console.WriteLine("Сервис запущен");
+                        Console.ReadLine();
+                        svc.Stop();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (Environment.UserInteractive)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    // @@@ Дописать протоколирование
+                }
+            }
+        }
+    }
+}
