1202d8a1c5e78d0ea43d99bd1f185def57d3b117
Sergey Bannikov <6384@croc.ru>
2019-04-29 21:06:45 +0300

    Сервис операционной системы

---
+++ SeaBattle/SeaBattleService/EventID.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle
+{
+    public enum EventID
+    {
+        None,
+        Start,
+        Stop,
+        Pause,
+        Continue,
+        Timer,
+        Exception
+    }
+}
